f413440f6a7a69358e9b1856368f6837c77a7b3f
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2023-07-14 18:19:39 -0500

    add firestore

---
+++ src/src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'
+
+const firebase = initializeApp({
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET, 
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID, 
+  appId: import.meta.env.APP_ID,
+});
+
+export const firestore = getFirestore(firebase)
